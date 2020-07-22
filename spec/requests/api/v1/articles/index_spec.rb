d62793b7199f1266bfd5fa1f74f81ec7c4ef3f84
felixoppongwillborg <fwlixw@gmail.com>
2020-07-22 20:35:44 +0200

    wrote first request spec for articles index no test passing

---
+++ spec/requests/api/v1/articles/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_heper'
+
+RSpec.describe "GET /v1/articles", type: :request do
+  describe'successfully gets articles' do
+    let!(:articles) { 3.times{ create(:articles)} }
+    before do
+      get'/api/v1/articles'
+    end
+    it'should return a 200 response' do
+      expect(response).to have_http_status 200
+    end
+    it'should return article' do
+      expect(response_json["articles"].count).to eq 3
+    end
+    it'should return title' do
+      expect(response_json["articles"].first["title"]).to have_content "News on Rails is live"
+    end
+    it'should return lead' do
+      expect(response_json["articles"].first["lead"]).to have_content "Yes it is correct"
+    end
+  end
+end
