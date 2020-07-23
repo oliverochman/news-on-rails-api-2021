0a76633d9c7ca03dc599042a1305e1178201a744
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-23 16:27:16 +0200

    Fixing formatting in request spec

--- spec/requests/api/v1/articles/index_spec.rb
+++ spec/requests/api/v1/articles/index_spec.rb
@@ -1,18 +1,18 @@
 RSpec.describe "GET /v1/articles", type: :request do
-  describe'successfully gets articles' do
+  describe 'successfully gets articles' do
     let!(:article_1) { create(:article, title: 'The first article', lead: 'This is the first article lead') }
     let!(:article_2) { create(:article, title: 'The second article', lead: 'This is the second article lead') }
     let!(:article_3) { create(:article, title: 'The third article', lead: 'This is the third article lead') }
 
     before do
-      get'/api/v1/articles'
+      get '/api/v1/articles'
     end
 
-    it'should return a 200 response' do
+    it 'should return a 200 response' do
       expect(response).to have_http_status 200
     end
 
-    it'should return article' do
+    it 'should return article' do
       expect(response_json["articles"].count).to eq 3
     end
 
@@ -25,12 +25,12 @@ RSpec.describe "GET /v1/articles", type: :request do
     end
   end
 
-  describe'no articles has been added' do
+  describe 'no articles has been added' do
     before do
-      get'/api/v1/articles'
+      get '/api/v1/articles'
     end
 
-    it'should have no articles on page' do
+    it 'should have no articles on page' do
       expect(response_json["articles"]).to eq []
     end
   end
