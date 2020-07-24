edb05ca43321dc780fca6ded2db8204d8e43e7cc
felixoppongwillborg <fwlixw@gmail.com>
2020-07-24 13:47:23 +0200

    started writing a new test for user can request articles in specific category

--- spec/requests/api/v1/articles/show_article_spec.rb
+++ spec/requests/api/v1/articles/show_article_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe "GET /v1/articles", type: :request do
-  let!(:article) { create(:article, title: 'The first article', lead: 'This is the first article lead', content: 'This is the first article content', category: 0) }
+  let!(:article) { create(:article, title: 'The first article', lead: 'This is the first article lead', content: 'This is the first article content', category: "sports") }
 
   describe 'successfully gets article' do
     before do
---
+++ spec/requests/api/v1/articles/user_can_request_articles_in_specific_category_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe'GET /api/v1/articles/', type: :request do
+  let!(:article) { create(:article, category: 0) }
+  let!(:article1) { create(:article, category: 0) }
+  let!(:article2) { create(:article, category: 1) }
+
+  describe'successfully'do
+  before do
+    get "/api/v1/articles",
+    params:(category: "sports")
+  end
+    it'responds with a 200 status'do
+    expect(response).to have_http_status 200
+    end
+
+    it 'should return articles with category sports' do
+      expect(response_json["articles"].count).to eq 2
+    end
+  end
+end
