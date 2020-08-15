6bc5017eed38b191249b8bd98dd0d0b3c3c26f50
Sima <sima.boreiri@gmail.com>
2020-08-15 12:31:53 +0200

    adds test for sad path and modifies the controller

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,52 +1,60 @@
 class Api::V1::SubscriptionsController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :user_is_subscriber?
+  before_action :check_stripe_token
+
   def create
     begin
-    customer_id = get_customer(params[:stripeToken])
+      customer_id = get_customer(params[:stripeToken])
 
-    subscription = Stripe::Subscription.create({
-       customer: customer_id,
-       plan: 'nor_subscription_plan'
+      subscription = Stripe::Subscription.create({ 
+        customer: customer_id, 
+        plan: 'nor_subscription_plan' 
       })
-      
-      test_env?(customer_id, subscription)
 
+      test_env?(customer_id, subscription)
 
       status = Stripe::Invoice.retrieve(subscription.latest_invoice).paid
 
-      if status 
+      if status
         current_user.update_attribute(:role, 'subscriber')
         render json: {message: 'Transaction was successfull'}
-
       else
         render_stripe_error('Transaction was unsuccessfull')
-
       end
-
     rescue => error
       render_stripe_error(error.message)
-
     end 
   end
 
-  private 
+  private
 
   def get_customer(stripeToken)
     customer = Stripe::Customer.list(email: current_user.email).data.first
-    customer ||= Stripe::Customer.create({email: current_user.email, source: stripeToken })
+    customer ||= Stripe::Customer.create({ email: current_user.email, source: stripeToken })
 
     customer.id
   end
 
   def render_stripe_error(error)
-    render json: {message: "Something went wrong. #{error}"}, status: 422
+    render json: { message: "Something went wrong. #{error}" }, status: 422
   end
 
   def test_env?(customer, subscription)
     if Rails.env.test?
-      invoice = Stripe::Invoice.create({ customer: customer, subscription: subscription.id, paid: true})
+      invoice = Stripe::Invoice.create({ customer: customer, subscription: subscription.id, paid: true })
       subscription.latest_invoice = invoice.id
     end
+  end
+
+  def check_stripe_token
+    if !params[:stripeToken] or params[:stripeToken].empty?
+      render_stripe_error('There was no token provided...')
+      return
+    end
+  end
+
+  def user_is_subscriber?
+    current_user.subscriber? && render_stripe_error('You are already a subscriber') and return
   end
 end
--- spec/requests/api/v1/subscriptions/create_spec.rb
+++ spec/requests/api/v1/subscriptions/create_spec.rb
@@ -1,25 +1,24 @@
 require 'stripe_mock'
 
 RSpec.describe "POST /v1/subscriptions", type: :request do
-  let(:stripe_helper) { StripeMock.create_test_helper}
-  before(:each) {StripeMock.start}
-  after(:each)  {StripeMock.stop}
-  let(:valid_token) { stripe_helper.generate_card_token}
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  before(:each) { StripeMock.start }
+  after(:each) { StripeMock.stop }
+  let(:valid_token) { stripe_helper.generate_card_token }
 
-  let(:product) { stripe_helper.create_product}
+  let(:product) { stripe_helper.create_product }
   let!(:plan) do
     stripe_helper.create_plan(
       id: "nor_subscription_plan",
-      amount: 149,
+      amount: 500,
       currency: "usd",
       interval: "month",
       interval_count: 12,
-      name: "News On Rails Subscription",
+      name: 'News On Rails Subscription',
       product: product.id
-
     )
-
   end
+
 
   let(:user) { create(:user, role: 'registered')}
   let(:credentials) { user.create_new_auth_token }
@@ -34,17 +33,99 @@ RSpec.describe "POST /v1/subscriptions", type: :request do
       headers: headers
     end
 
-    it 'is expected  to return a 200 response status' do
-      expect(response).to have_http_status 200 
+    it 'is expected to return 200 response status' do
+      expect(response).to have_http_status 200
     end
 
-    it 'is expected to return a success message' do
+    it 'is expected to return success message' do
       expect(response_json["message"]).to eq "Transaction was successfull"
     end
 
     it 'is expected to change user role to subscriber' do
       user.reload
       expect(user.role).to eq 'subscriber'
+    end
+  end
+
+  describe 'unsuccessfully when' do
+    describe "with invalid parameters" do
+      before do
+        post "/api/v1/subscriptions", headers: headers
+      end
+
+      it "returns error message" do
+        expect(response_json["message"]).to eq "Something went wrong. There was no token provided..."
+      end
+
+      it "returns error http code" do
+        expect(response).to have_http_status 422
+      end
+
+      it "does NOT set user role to subsciber" do
+        user.reload
+        expect(user.role).not_to eq 'subscriber'
+      end
+    end
+
+    describe "stripeToken is empty" do
+      before do
+        post "/api/v1/subscriptions",
+             params: { stripeToken: "" }, headers: headers
+      end
+
+      it "returns a error http code" do
+        expect(response).to have_http_status 422
+      end
+
+      it "does NOT set user role to subsciber" do
+        user.reload
+        expect(user.role).not_to eq 'subscriber'
+      end
+
+      it "returns an error message" do
+        expect(response_json["message"]).to eq "Something went wrong. There was no token provided..."
+      end
+    end
+
+    describe "credit card is declined" do
+      before do
+        StripeMock.prepare_card_error(:card_declined, :new_invoice)
+
+        post "/api/v1/subscriptions",
+          params: { stripeToken: valid_token }, headers: headers
+      end
+
+      it "returns a error http code" do
+        expect(response).to have_http_status 422
+      end
+
+      it "does NOT set user role to subsciber" do
+        user.reload
+        expect(user.role).not_to eq 'subscriber'
+      end
+
+      it "returns an error message" do
+        expect(response_json["message"]).to eq "Something went wrong. The card was declined"
+      end
+    end
+
+    describe "user is already subscriber" do
+      let(:subscriber) { create(:user, role: :subscriber) }
+      let(:subscriber_credentials) { subscriber.create_new_auth_token }
+      let(:subscriber_headers) { { HTTP_ACCEPT: "application/json" }.merge!(subscriber_credentials) }
+
+      before do
+        post "/api/v1/subscriptions",
+          params: { stripeToken: valid_token }, headers: subscriber_headers
+      end
+
+      it "returns a error http code" do
+        expect(response).to have_http_status 422
+      end
+
+      it "returns an error message" do
+        expect(response_json["message"]).to eq "Something went wrong. You are already a subscriber"
+      end
     end
   end
 end
