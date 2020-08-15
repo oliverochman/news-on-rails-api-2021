090754732649ffc529c49024c07e1ddee3b267c7
Sima <sima.boreiri@gmail.com>
2020-08-15 10:27:29 +0200

    adds route for create sunbscription

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create],  constraints: { format: 'json' }
+      resources :sunbscriptions, only: [:create], constraints: { format: 'json' }
     end
   end
 end
