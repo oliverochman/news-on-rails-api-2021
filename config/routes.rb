ecec5845f882a3043a0eb3bee85a7c0ee6ce4236
Sima <sima.boreiri@gmail.com>
2020-08-15 10:33:20 +0200

    corrects typo

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create],  constraints: { format: 'json' }
-      resources :sunbscriptions, only: [:create], constraints: { format: 'json' }
+      resources :subscriptions, only: [:create], constraints: { format: 'json' }
     end
   end
 end
